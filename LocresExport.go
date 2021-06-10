7b1141091b527c6c6d1a2ce6226e68e52c016a36
Daisuke Yamamoto <d.yamamoto@bitgroove.jp>
2021-06-10 21:06:07 +0900

    エクセルに合わせて修正

--- LocresExport.go
+++ LocresExport.go
@@ -10,11 +10,12 @@ import (
 )
 
 //TagCol エクセルのtag記述行
-const TagCol = 1
+const TagCol = 2
 
 //Key key列を示すタグ文字
 const Key = "key"
 
+// contains スライスに文字列が存在するか判定
 func contains(slice []string, str string) bool {
 	for _, v := range slice {
 		if v == str {
@@ -22,6 +23,13 @@ func contains(slice []string, str string) bool {
 		}
 	}
 	return false
+}
+
+// directoryExists 指定したディレクトリが無ければ作成する
+func directoryExists(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		os.Mkdir(dir, 0777)
+	}
 }
 
 func main() {
@@ -32,22 +40,18 @@ func main() {
 	)
 	flag.Parse()
 
-	//翻訳対象の言語を読み込み
+	//翻訳対象の言語を設定
 	cultureCodes := strings.Split(*targetCultures, ",")
 	cultureCol := map[string]int{}
 
 	var keyCol int
 	//エクセルを読み込む
 	for _, Arg := range flag.Args() {
-		//Argに実行オプションも含まれるので無視する
+		//Argに実行オプションも含まれるので、ハイフンから始まるものは無視する
 		if !strings.HasPrefix(Arg, "-") {
 			excel, err := excelize.OpenFile(Arg)
 			if err != nil {
 				log.Fatal(err)
-			}
-			//エクセルのファイル名取得
-			if strings.Contains(Arg, "\\") {
-				Arg = Arg[strings.LastIndex(Arg, "\\"):]
 			}
 
 			var manifestOutput string
@@ -60,27 +64,29 @@ func main() {
 	"FormatVersion": 1,
 	"Namespace": "",
 	"Children": [`
+
 				for _, sheet := range excel.GetSheetList() {
 					rows, _ := excel.GetRows(sheet)
-					for col, cellValue := range rows[TagCol] {
+					for col, cellValue := range rows[TagCol-1] {
 						//key列と各言語の列番号を取得
 						if contains(cultureCodes, cellValue) {
-							cultureCol[cellValue] = col
-						} else if strings.Index(cellValue, Key) != -1 {
-							keyCol = col
+							cultureCol[cellValue] = col + 1
+						} else if strings.Contains(cellValue, Key) {
+							keyCol = col + 1
 						}
 					}
 
 					var i int
 					//タグが入らないようにTagCol+1から
-					for i = TagCol; i < len(rows); i++ {
-						keyCell, _ := excelize.CoordinatesToCellName(keyCol+1, i)
+					for i = TagCol + 1; i < len(rows); i++ {
+						keyCell, _ := excelize.CoordinatesToCellName(keyCol, i)
 						keyValue, err := excel.GetCellValue(sheet, keyCell)
 						if err != nil {
 							log.Fatal(err)
 						}
+						//keyの先頭がアンダースコアの場合は無視する
 						if keyValue != "" && !strings.HasPrefix(keyValue, "_") {
-							cultureCell, _ := excelize.CoordinatesToCellName(cultureCol[cultureCode]+1, i)
+							cultureCell, _ := excelize.CoordinatesToCellName(cultureCol[cultureCode], i)
 							cultureValue, err := excel.GetCellValue(sheet, cultureCell)
 							if err != nil {
 								log.Fatal(err)
@@ -121,6 +127,12 @@ func main() {
 	]
 }`
 
+				//エクセルのファイル名取得
+				//windows以外で実行したらバグる(多分)
+				if strings.Contains(Arg, "\\") {
+					Arg = Arg[strings.LastIndex(Arg, "\\"):]
+				}
+				directoryExists(*outputDir + "/" + cultureCode)
 				archiveFile, err := os.Create(*outputDir + "/" + cultureCode + "/" + strings.TrimRight(Arg, ".xlsx") + ".archive")
 				if err != nil {
 					log.Fatal(err)
